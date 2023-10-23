00501e093687e9a38f9b7d9554300f458ed35dd5
Israel Onoja <Israelonoja21@gmail.com>
2023-10-24 01:10:20 +0200

    add journal html

--- public/components/Journal.jsx
+++ public/components/Journal.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
 
 export default function Journal() {
   return (
-    <div>
-      
-    </div>
-  )
+    <section className="journal">
+      <img src="https://source.unsplash.com/WLxQvbMyfas" alt="Japan" />
+      <div className="journal--details">
+        <div className="journal--location">
+          <img
+            className="location--icon"
+            src="../images/Fill 219.png"
+            alt="location icon"
+          />
+          <span className="location--name">JAPAN</span>
+          <span className="location--link">
+            <a href="#">View on Google Maps</a>
+          </span>
+        </div>
+        <h2 className="journal--title">Mount Fuji</h2>
+        <p className="journal--date">12 Jan, 2021 - 24 Jan, 2021</p>
+        <p className="journal--description">
+          Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters
+          (12,380 feet). Mount Fuji is the single most popular tourist site in
+          Japan, for both Japanese and foreign tourists.
+        </p>
+      </div>
+    </section>
+  );
 }
