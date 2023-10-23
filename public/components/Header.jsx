37edfa917757f21352bd827f2ad0b7a0c8c1fbb2
Israel Onoja <Israelonoja21@gmail.com>
2023-10-24 00:25:46 +0200

    update to header component

---
+++ public/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className='header'>
+      <img className='logo' src="../images/Fill 213.png" alt="Logo" />
+      <p className='logo--text'>my travel journal.</p>
+    </div>
+  )
+}
