a282fa0f2d52c143bc7afa241cb1455fb373db7c
Israel Onoja <Israelonoja21@gmail.com>
2023-10-24 01:13:47 +0200

    import journal component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { useState } from "react";
 import Header from "../public/components/Header";
+import Journal from "../public/components/Journal";
 
 function App() {
-    return (
+  return (
+    <div>
       <Header />
-    )
+      <Journal />
+    </div>
+  );
 }
 
 export default App;
